80ff9b410db28f8366a18c4fcfed702afcdde5d8
Coder-sam-007 <92518021+Coder-sam-007@users.noreply.github.com>
2022-06-17 18:31:24 +0530

    version 1.0

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+let port = 3000;
+
+const users = [];
+
+server.listen(port, () => {
+  console.log("listening at port 3000");
+});
+
+io.on("connection", (socket) => {
+  // If any new user joins, let other users connected to the server know!
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  // If someone sends a message, broadcast it to other people
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+
+  // If someone leaves the chat, let others know
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
